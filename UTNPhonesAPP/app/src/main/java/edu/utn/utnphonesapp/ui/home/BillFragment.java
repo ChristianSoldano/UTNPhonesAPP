01b99b1190c8a438c476bba6aeb8838665e06f9a
ChristianSoldano <soldanochristian@hotmail.com>
2020-07-08 12:17:02 -0300

    last commit

--- UTNPhonesAPP/app/src/main/java/edu/utn/utnphonesapp/ui/home/BillFragment.java
+++ UTNPhonesAPP/app/src/main/java/edu/utn/utnphonesapp/ui/home/BillFragment.java
@@ -2,6 +2,10 @@ package edu.utn.utnphonesapp.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,12 +13,6 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +22,6 @@ import edu.utn.utnphonesapp.LoginActivity;
 import edu.utn.utnphonesapp.MainActivity;
 import edu.utn.utnphonesapp.R;
 import edu.utn.utnphonesapp.adapter.BillAdapter;
-import edu.utn.utnphonesapp.adapter.PhoneLineAdapter;
-
 import edu.utn.utnphonesapp.model.Bill;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -38,10 +34,15 @@ import static edu.utn.utnphonesapp.config.Constants.API_ROOT_URL;
 
 public class BillFragment extends Fragment {
 
+
+    private ConstraintLayout progressBar;
+    private ConstraintLayout noContent;
+
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
-    private ConstraintLayout progressBar;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,10 +57,11 @@ public class BillFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         progressBar = view.findViewById(R.id.loadingScreen);
+        noContent = view.findViewById(R.id.noContent);
         recyclerView = view.findViewById(R.id.phoneLineRecycleView);
         recyclerView.setHasFixedSize(true);
-
         layoutManager = new LinearLayoutManager(getContext());
+
 
         getBills();
     }
@@ -94,6 +96,7 @@ public class BillFragment extends Fragment {
 
                 if (response.code() == 204) {
                     adapter = new BillAdapter(new ArrayList<Bill>());
+                    noContent.setVisibility(View.VISIBLE);
                 } else
                     adapter = new BillAdapter(response.body());
 
